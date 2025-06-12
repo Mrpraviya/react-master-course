f850d474cbfedcc239752f93a807a05ad2d54c3b
Yousof <sandeeptha2001praveen@gmail.com>
2025-06-13 03:22:22 +0530

    JS update

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-// // External java script file
+// External java script file
 
-// var userEmail = "sandeeptha2001praveen@gmail.com";
+var userEmail = "sandeeptha2001praveen@gmail.com";
 
-// const userName = "Sandeeptha Praveen";
+const userName = "Sandeeptha Praveen";
 
-// let userAge = 23 ;
+let userAge = 23 ;
 
-// //This is single line comments
+//This is single line comments
 
-// /*This 
-//     is
-//         multi
-//             line comments*/
+/*This 
+    is
+        multi
+            line comments*/
 
-// userAge = 29;
+userAge = 29;
 
-// console.log("Personal details : ", {userName,userEmail,userAge});
+console.log("Personal details : ", {userName,userEmail,userAge});
