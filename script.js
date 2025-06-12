0871b3921ffb7f163e30984ca7a80c6828fc0fa8
Yousof <sandeeptha2001praveen@gmail.com>
2025-06-13 03:20:50 +0530

    JS file creation

---
+++ script.js
@@ -0,0 +1,18 @@
+// // External java script file
+
+// var userEmail = "sandeeptha2001praveen@gmail.com";
+
+// const userName = "Sandeeptha Praveen";
+
+// let userAge = 23 ;
+
+// //This is single line comments
+
+// /*This 
+//     is
+//         multi
+//             line comments*/
+
+// userAge = 29;
+
+// console.log("Personal details : ", {userName,userEmail,userAge});
