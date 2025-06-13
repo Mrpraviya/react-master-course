8af87d6b268799aded76bc2f7eb4d3b8c83cbac4
Yousof <sandeeptha2001praveen@gmail.com>
2025-06-13 15:17:08 +0530

    JS variable types

--- script.js
+++ script.js
@@ -13,6 +13,32 @@ let userAge = 23 ;
         multi
             line comments*/
 
-userAge = 29;
+userAge = 29; // Number
 
-console.log("Personal details : ", {userName,userEmail,userAge});
+const isAuthenticated = false;//Boolean
+const lastName = null;
+const firstName = undefined;
+const userData = {
+    firstName : "Sandeeptha",
+    lastName : "Praveen",
+    age : 24,
+};
+
+//Arrays
+
+const fruits = ["banana", "apple", "Mango"] ; // string array
+const Number = [1, 2, 3, 4]; // Number array
+
+const users = [
+    {id: 1, name: "Sandeeptha"}, {id: 2, name: "Praveen"}, {id: 3, name: "Kavindu"}, 
+] // Object Array
+ 
+
+// console.log("Personal details : ", {userName,userEmail,userAge});
+
+// console.log( userEmail, typeof userEmail);
+// console.log( userAge, typeof userAge);
+// console.log( isAuthenticated, typeof isAuthenticated);
+// console.log( fruits, typeof fruits);
+// console.log( Number, typeof Number);
+console.log( users, typeof users);
